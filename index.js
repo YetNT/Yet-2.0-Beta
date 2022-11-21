1f7db50b3b8142658a7e0e8e778c15497c50a151
Yet <78223322+Yetity@users.noreply.github.com>
2022-11-21 14:02:26 +0000

    added main aoi and database

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const port = 3000;
 app.get('/', (req, res) => res.send("helo wrldo"));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)); 
+
+const aoijs = require("aoi.js");
+const bot = new aoijs.AoiClient({
+  token: process.env.BETA,
+  prefix: ["yb!",'yet!','yb! ','YB!','YB! '],
+  intents: ["MessageContent", "Guilds", "GuildMessages"]
+});
+
+
+bot.onMessage();
+bot.onInteractionCreate();
